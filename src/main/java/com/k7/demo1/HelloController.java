ff71b327bb453908b141242536c067bfcc7da7d4
Kevin Guan <k7sem@126.com>
2019-01-08 14:06:01 +0800

    changs

---
+++ src/main/java/com/k7/demo1/HelloController.java
@@ -0,0 +1,15 @@
+package com.k7.demo1;
+
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello Kevin from Spring Boot!";
+    }
+
+}
